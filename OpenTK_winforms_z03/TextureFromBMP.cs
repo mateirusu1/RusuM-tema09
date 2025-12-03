b727b1db44166458eee87183ec28d89fc27144f3
mateirusu1 <matei.rusu1@student.usv.ro>
2025-12-03 22:48:44 +0200

    Add files via upload

---
+++ OpenTK_winforms_z03/TextureFromBMP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_winforms_z03
+{
+    class TextureFromBMP
+    {
+        public int id;
+        public int width;
+        public int height;
+
+        public TextureFromBMP(int _id, int _width, int _height)
+        {
+            id = _id;
+            width = _width;
+            height = _height;
+        }
+    }
+}
